950582107298b8b5cb801105a525e56a7c74cf54
Paul Won <wonpaul2223@gmail.com>
2022-05-18 19:54:54 -0500

    more updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // requring the necessary directories
-require('dotenv').config();
+require("dotenv").config();
 const mysql = require("mysql2");
 const inq = require("inquirer");
 const cTable = require("console.table");
@@ -9,7 +9,7 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: process.env.DB_PASSWORD,
-  database: "employees_db"
+  database: "employees_db",
 });
 
 db.connect(function (err) {
@@ -62,7 +62,7 @@ const initialQuestions = () => {
           break;
 
         case "Add Employee":
-          addEmployee();
+          //   addEmployee();
 
           break;
 
@@ -93,12 +93,24 @@ const initialQuestions = () => {
 
         case "Quit":
           quit();
+
           break;
       }
     });
 };
 
+// displays all the employees
 const viewEmployee = () => {
+  console.log(`
+    ______________________________________
+    |                                    |
+    |         ================           |
+    |      ** Viewing Employee **        |
+    |         ================           |
+    |                                    |
+    |____________________________________|
+    `);
+
   const sql = "SELECT * FROM employee";
 
   db.query(sql, (err, results) => {
@@ -111,6 +123,7 @@ const viewEmployee = () => {
   });
 };
 
+// adding an employee
 const addEmployee = () => {
   console.log(`
     ______________________________________
@@ -121,67 +134,67 @@ const addEmployee = () => {
     |                                    |
     |____________________________________|
     `);
-  inq
-    .prompt([
-      {
-        message: "Enter employee first name",
-        name: "firstname",
-        type: "input",
-      },
-      {
-        message: "Enter employee last name",
-        name: "lastname",
-        type: "input",
-      },
-      {
-        message: "Please choose a role for employee",
-        name: "rolechoice",
-        type: "list",
-        choices: [
-          "Hiring Mananger",
-          "Personnel Manager",
-          "Junior Software Engineer",
-          "Products Engineer",
-          "Marketing Analyst",
-          "Sales Associate",
-          "Cusomter Service Representative",
-          "Administrative Assistant",
-          "Office clerk",
-        ],
-      },
-      {
-        message: "Please enter manager ID",
-        name: "managerchoice",
-        type: "list",
-        choices: ["None", "1", "2", "3", "4", "5"],
-      },
-    ])
-    .then((data) => {
-      const firstName = data.firstname;
-      const lastName = data.lastname;
-      const roleChoice = data.rolechoice;
-      const manangerChoice = data.managerchoice;
 
-      // the question marks serve as placeholders
-      const sql =
-        "INSERT INTO employee_role (first_name, last_name, title) VALUES (?,?,?)";
+  const sql = "SELECT * FROM employee_role";
 
-      db.query(sql, [firstName, lastName, roleChoice], (err, results) => {
-        if (err) {
-          console.log(err);
-        } else {
-          viewEmployee();
-          initialQuestions();
-        }
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+
+    console.log(results.map((x) => x.title));
+    const roles = results.map((x) => x.title);
+
+    inq
+      .prompt([
+        {
+          message: "Enter employee first name",
+          name: "firstname",
+          type: "input",
+        },
+        {
+          message: "Enter employee last name",
+          name: "lastname",
+          type: "input",
+        },
+        {
+          message: "Please select a role for employee",
+          name: "rolechoice",
+          type: "list",
+          choices: roles,
+        },
+        {
+          message: "Please enter manager ID",
+          name: "managerchoice",
+          type: "list",
+          choices: ["None", "1", "2", "3", "4", "5"],
+        },
+      ])
+      .then((data) => {
+        const firstName = data.firstname;
+        const lastName = data.lastname;
+        const roleChoice = data.rolechoice;
+        const managerChoice = data.managerchoice;
+
+        const sql = "INSERT INTO";
       });
-    });
+  });
 };
 
 // const updateRole = () => {
 
 // }
 
+// displays all the roles
 const viewRoles = () => {
+  console.log(`
+    ______________________________________
+    |                                    |
+    |         ================           |
+    |        ** Viewing Roles **         |
+    |         ================           |
+    |                                    |
+    |____________________________________|
+    `);
+
   const sql = "SELECT id, title, salary FROM employee_role";
 
   db.query(sql, (err, results) => {
@@ -194,7 +207,18 @@ const viewRoles = () => {
   });
 };
 
+// adds role to the list of roles
 const addRole = () => {
+  console.log(`
+    ______________________________________
+    |                                    |
+    |         ================           |
+    |        ** Adding Roles **          |
+    |         ================           |
+    |                                    |
+    |____________________________________|
+    `);
+
   const sql = "SELECT * FROM departments";
 
   db.query(sql, (err, results) => {
@@ -244,7 +268,18 @@ const addRole = () => {
   });
 };
 
+// adds department to list of departments
 const addDepartments = () => {
+    console.log(`
+    ______________________________________
+    |                                    |
+    |       ====================         |
+    |     ** Adding Departments **       |
+    |       ====================         |
+    |                                    |
+    |____________________________________|
+    `);
+
   inq
     .prompt([
       {
@@ -270,28 +305,31 @@ const addDepartments = () => {
     });
 };
 
+// displays all departments
 const viewDepartments = () => {
+    console.log(`
+    ______________________________________
+    |                                    |
+    |         ===============            |
+    |        ** DEPARTMENTS **           |
+    |         ===============            |
+    |                                    |
+    |____________________________________|
+    `);
+
   const sql = "SELECT * FROM departments";
 
   db.query(sql, (err, results) => {
     if (err) {
       console.log(err);
     } else {
-      console.log(`
-    ______________________________________
-    |                                    |
-    |          ==============            |
-    |         ** Departments **          |
-    |          ==============            |
-    |                                    |
-    |____________________________________|
-    `);
       console.table(results);
       initialQuestions();
     }
   });
 };
 
+// ends the application
 const quit = () => {
   console.log(`
     ______________________________________
@@ -303,4 +341,3 @@ const quit = () => {
     |____________________________________|
     `);
 };
-
